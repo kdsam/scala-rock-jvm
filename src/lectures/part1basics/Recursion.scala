2b8430f7a8c11d9a52326530d5e0ef3b38c38946
kdsam <kdmandawe06@gmail.com>
2019-01-24 09:05:47 +0800

    fibo tailrec

--- src/lectures/part1basics/Recursion.scala
+++ src/lectures/part1basics/Recursion.scala
@@ -66,4 +66,15 @@ object Recursion extends App {
 
   println(isPrime(2003))
   println(isPrime(629))
+
+  def fibonacci(n: Int): Int = {
+    def fiboTailRec(i: Int, last: Int, nextToLast: Int): Int =
+      if (i >= n) last
+      else fiboTailRec(i + 1, last + nextToLast, last)
+
+    if (n <= 2) 1
+    else fiboTailRec(2, 1, 1)
+  }
+
+  println(fibonacci(8)) // 1 1 2 3 5 8 13 21
 }
