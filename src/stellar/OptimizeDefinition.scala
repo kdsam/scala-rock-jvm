5d4814d707dbf8673c8ecbc25de04cf9fc3acb43
kdsam <kdmandawe06@gmail.com>
2019-03-04 11:38:52 +0800

    -

--- src/stellar/OptimizeDefinition.scala
+++
@@ -1,8 +0,0 @@
-package stellar
-
-sealed trait OptimizeDefinition {
-  def ifPresent(expr: Expr): Boolean
-  def applyOptimization(expr: Expr): Expr
-}
-
-abstract class WhereOptimizeDefinition extends OptimizeDefinition
